a172b2ae9df38c696eae82518870f2169fc5ff8f
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-05 02:28:25 +0000

    conlang add process

---
+++ app/dashboard/create_conlang/page.tsx
@@ -0,0 +1,133 @@
+'use client'
+import { supabase } from "@/lib/supabase/database";
+import { InfoIcon, Router } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function CreateConlangPage({ params }: { params: { id: string } }) {
+    const router = useRouter();
+
+    const [conlang, setConlang] = useState({
+        english_name: "",
+        code: "",
+        summary: "",
+        native_name: ""
+    });
+
+    //@ts-ignore
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setConlang(prevConlang => ({
+            ...prevConlang,
+            [name]: name === 'code' ? value.toUpperCase() : value
+        }));
+    };
+
+    //@ts-ignore
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { error } = await supabase.from('conlang').insert([conlang]);
+
+            if (error) {
+                throw error;
+            }
+
+            router.push(`/dashboard/view/${conlang.code}`);
+        } catch (err) {
+            console.error('Error:', err);
+        }
+
+    };
+
+    return <div className="flex-1 w-full flex flex-col gap-12">
+        <div className="w-full">
+            <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-8 items-center">
+                <InfoIcon size="16" strokeWidth={2} />
+                In this page you can add your conlang to the website. Fell free to add all other of your conlangs if you have more than one.
+            </div>
+            <div className="flex w-full flex-col gap-2 mt-8">
+                <span className="text-2xl"><strong>Create a Conlang</strong></span>
+            </div>
+            <form onSubmit={handleSubmit} className="max-w-xl mx-auto p-8 bg-gray-100 dark:bg-gray-800 rounded-xl shadow-lg">
+                <div className="space-y-6">
+
+                    {/* English Name */}
+                    <div>
+                        <label htmlFor="english_name" className="block text-sm font-semibold text-gray-700 dark:text-gray-300 mb-1">English Name</label>
+                        <input
+                            type="text"
+                            name="english_name"
+                            id="english_name"
+                            value={conlang.english_name}
+                            onChange={handleChange}
+                            required
+                            placeholder="E.g., Na'vi"
+                            className="block w-full rounded-lg border-gray-300 dark:border-gray-600 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm p-2 bg-white dark:bg-gray-700 dark:text-gray-200"
+                        />
+                    </div>
+
+                    {/* Native Name */}
+                    <div>
+                        <label htmlFor="native_name" className="block text-sm font-semibold text-gray-700 dark:text-gray-300 mb-1">Native Name</label>
+                        <input
+                            type="text"
+                            name="native_name"
+                            id="native_name"
+                            required
+                            value={conlang.native_name}
+                            onChange={handleChange}
+                            placeholder="E.g., Lì'fya leNa'vi"
+                            className="block w-full rounded-lg border-gray-300 dark:border-gray-600 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm p-2 bg-white dark:bg-gray-700 dark:text-gray-200"
+                        />
+                    </div>
+
+                    {/* Conlang Code */}
+                    <div>
+                        <label htmlFor="code" className="block text-sm font-semibold text-gray-700 dark:text-gray-300 mb-1">Code</label>
+                        <input
+                            type="text"
+                            name="code"
+                            required
+                            id="code"
+                            value={conlang.code}
+                            onChange={handleChange}
+                            maxLength={6}
+                            placeholder="E.g., nav"
+                            className="block w-full rounded-lg border-gray-300 dark:border-gray-600 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm p-2 bg-white dark:bg-gray-700 dark:text-gray-200"
+                        />
+                        <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">A short, unique code for the language (e.g., ISO 639-3 style).</p>
+                    </div>
+
+                    {/* Summary */}
+                    <div>
+                        <label htmlFor="summary" className="block text-sm font-semibold text-gray-700 dark:text-gray-300 mb-1">Summary</label>
+                        <textarea
+                            name="summary"
+                            required
+                            id="summary"
+                            rows={4}
+                            value={conlang.summary}
+                            onChange={handleChange}
+                            maxLength={2000}
+                            placeholder="Briefly describe the conlang, its purpose, and key features."
+                            className="block w-full rounded-lg border-gray-300 dark:border-gray-600 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm p-2 bg-white dark:bg-gray-700 dark:text-gray-200"
+                        ></textarea>
+                    </div>
+
+                    {/* Submit Button */}
+                    <div>
+                        <button
+                            type="submit"
+                            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200"
+                        >
+                            Add Conlang
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+}
---
+++ app/dashboard/view/[id]/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+import { supabase } from "@/lib/supabase/database";
+import { InfoIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function ViewConlangPage({ params }: { params: { id: string } }) {
+  const [conlang, setConlang] = useState({
+    english_name: "",
+    id: "",
+    code: "",
+    created_at: "",
+    created_by: "",
+    summary: "",
+    native_name: ""
+  });
+
+  const { id } = params;
+
+  useEffect(() => {
+    const getConlangFromId = async () => {
+      const conlangs = await supabase.from('conlang').select('*').eq('code', id);
+
+      let data = await conlangs?.data;
+
+      if (data?.length! > 0) {
+        console.log(data![0])
+        setConlang(data![0]);
+      }
+    }
+
+    getConlangFromId();
+
+  }, [])
+
+  return <div className="flex-1 w-full flex flex-col gap-12">
+    <div className="w-full">
+      <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-8 items-center">
+        <InfoIcon size="16" strokeWidth={2} />
+        Here you can find details about this language. Feel free to explore and create your own conlangs!
+      </div>
+      <div className="flex w-full flex-col gap-2 mt-8">
+        <span className="text-2xl"><strong>{conlang?.english_name ? conlang?.english_name : 'Unnamed'}</strong></span>
+        <span className="text-m">({conlang?.native_name})</span>
+        <span className="text-sm">This language has been created by <a href="/">{conlang?.created_by}</a> on {conlang?.created_at}</span>
+      </div>
+      <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Edit Information</button>
+      <hr />
+      <p className="mt-8">
+        {conlang?.summary}
+      </p>
+      <hr className="my-8"/>
+      <span className="text-xl">Stats</span>
+      <div className="flex flex-col mt-2 w-full gap-4">
+        <span>This language has 0 words in its lexicon, 0 vowels, 0 consonants and 0 phrases.</span>
+        <span>It belongs to the Indo-European Family</span>
+      </div>
+      <hr className="my-8"/>
+      <span className="text-xl">Access Information</span>
+      <div className="flex w-full mt-2 flex-col">
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Typology</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Sample Texts</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Articles</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Phonology</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Morphology</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Syntax</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Semantics and Pragmatics</button>
+        <button className="bg-white my-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Dictionary</button>
+      </div>
+    </div>
+  </div>
+}
