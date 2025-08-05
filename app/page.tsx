d684100a2893ec81d76e234aa11fabe3b1728981
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-05 15:13:30 +0000

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Andrew Lingobase</Link>
+              <Link href={"/dashboard"}>Andrew Lingobase</Link>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
