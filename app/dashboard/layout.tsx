c96750c92dd61fddcd5664d57523e51dca6668de
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-06 02:13:57 +0000

    new sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import SidebarNav from "@/components/ui/navsidebar";
 
 export default function ProtectedLayout({
   children,
@@ -23,6 +24,7 @@ export default function ProtectedLayout({
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
+        <SidebarNav/>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
