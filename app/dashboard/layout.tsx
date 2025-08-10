0a6ee8111bfbaf07c6f79c0c46acde8ba1d8982c
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-10 19:44:09 +0000

    Navbar fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function ProtectedLayout({
   return (
       <main className="min-h-screen flex flex-col items-center bg-gray-100 dark:bg-gray-900">
         <div className="flex-1 w-full flex flex-col gap-20 items-center">
-          <nav className="w-full sticky top-0 z-10 flex justify-center border-b border-b-foreground/10 h-16">
+          <nav className="w-full sticky top-0 z-10 flex justify-center border-b border-b-foreground/10 h-16 bg-white dark:bg-gray-900 shadow-md">
             <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
               <div className="flex gap-5 items-center font-semibold">
                 <img src="/img/LINGOBASE_4.webp" width="32px" height="32px"/>
