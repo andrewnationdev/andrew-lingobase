5272c4ad0477b3816030c50bc0a8c1cf8d2d30c7
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-22 02:20:01 +0000

    ui change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -36,8 +36,7 @@ export default async function DashboardPage() {
       <div className="flex flex-col gap-2 items-start" id="your-conlangs">
         <h2 className="font-bold text-2xl mb-4">Your Conlangs:</h2>
         <ConlangsList authOnly user={uname} />
-        <Link className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200" href="/dashboard/create_conlang">Add Your Conlang</Link>
-        <Link className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200" href="/dashboard/view/DE-PLB">Sample Conlang</Link>
+        <Link className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200" href="/dashboard/create_conlang">Create a New Conlang</Link>
         <hr className="my-8" />
         <h2 className="font-bold text-2xl mb-4" id="conlangs-in-website">{`Everyone Else's Conlangs`}</h2>
         <ConlangsList />
