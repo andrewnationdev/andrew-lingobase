d8fb4f6debce4a40ab8fcbb568ed697cb5dded80
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-10-06 18:28:30 +0000

    new horizontal navbar

--- components/ui/navsidebar.tsx
+++ components/ui/navsidebar.tsx
@@ -20,20 +20,21 @@ const SidebarNav = ({ loggedUser }) => {
     { id: 6, icon: faQuestionCircle, link: "/dashboard/faq" }
   ];
 
+
   return (
-    <nav className="fixed left-4 bottom-4 w-16 bg-white/30 dark:bg-zinc-900 backdrop-blur-md p-2 rounded-2xl shadow-lg flex flex-col items-center space-y-4 md:top-1/2 md:-translate-y-1/2 md:bottom-auto">
+    <nav className="fixed left-1/2 bottom-4 -translate-x-1/2 w-auto bg-white/30 dark:bg-zinc-900 backdrop-blur-md p-2 rounded-2xl shadow-lg flex flex-row items-center space-x-4 md:top-auto md:bottom-4 md:left-1/2 md:-translate-x-1/2">
       {navItems.map((item) => (
         <Link
           key={item.id}
           href={item.link}
           className="
-    group w-5 h-5 md:w-6 md:h-6 lg:w-8 md:h-8 rounded-md flex items-center justify-center 
+    group w-8 h-8 rounded-md flex items-center justify-center 
     text-gray-600 hover:bg-gray-100 transition-all duration-200
   "
         >
           <FontAwesomeIcon
             icon={item.icon}
-            className="!text-sm transition-colors duration-200 text-teal-800 dark:text-sky-500 group-hover:text-blue-500"
+            className="!text-lg transition-colors duration-200 text-teal-800 dark:text-sky-500 group-hover:text-blue-500"
           />
         </Link>
       ))}
