fd2c63fb955c19fe3e985b42b2a7082f16e44254
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-10-06 23:00:48 -0300

    Update conlang-view.tsx

--- components/ui/conlang-view.tsx
+++ components/ui/conlang-view.tsx
@@ -163,14 +163,15 @@ export default function ViewConlang({ id, loggedUser }) {
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm mt-8 font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200"
               href={`/dashboard/create_conlang/${conlang?.code}`}
             >
-              Like ({numberOfLikes})
+              {`Like (${numberOfLikes})`}
             </button>
             <button
               onClick={() => {handleLikes(-1)}
               disabled={!ratingChosen}
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm mt-8 font-semibold text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200"
             >
-              Dislike ({numberOfDislikes})
+              {`Dislike (${numberOfDislikes})`}
+
             </button>
             <button
               onClick={() => window.alert("You don't have permission to comment yet!")}
