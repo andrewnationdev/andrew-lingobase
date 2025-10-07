94a1ff17d92beaf3782c202b0dee8eaeb8c239ed
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-10-07 01:38:36 +0000

    literature page

---
+++ app/dashboard/literature/page.tsx
@@ -0,0 +1,8 @@
+export default async function LiteraturePage() {
+    return <>
+    <div className="bg-accent-light text-gray-800 dark:bg-accent-dark dark:text-gray-300 text-sm p-3 px-5 rounded-md flex gap-3 items-center">
+  <InfoIcon size="16" strokeWidth={2} />
+  In the future, you will be able to add fanfics and short stories written in your conlangs here and showcase them :)
+</div>
+    </>
+}
