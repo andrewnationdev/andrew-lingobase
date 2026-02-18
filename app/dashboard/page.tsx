3fd48a332d66293ab63fe33dc73265994818cb27
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-02-18 18:51:28 +0000

    ✨ Join Discord from the Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,7 +24,7 @@ export default async function DashboardPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-4">
-      <LingobaseStats/>
+      <LingobaseStats />
       <QuickNavigationComponent
         data={[
           {
@@ -38,9 +38,20 @@ export default async function DashboardPage() {
           {
             href: "#conlangs-in-website",
             text: "Conlangs in the Website",
-          }
+          },
         ]}
       />
+      <div className="light:bg-orange-100 dark:bg-orange-800 shadow-lg p-4 flex justify-center space-x-6 rounded-lg">
+        <span className="font-bold text-lg">
+          Did you know we are on Discord too?
+        </span>
+        <Link
+          className="max-w-[150px] transition duration-150 ease-in-out hover:scale-110 w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-lg shadow-md text-sm font-semibold text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-colors duration-200"
+          href="https://discord.gg/6BEpySdDdv"
+        >
+          Join Now
+        </Link>
+      </div>
       <div className="flex flex-col gap-2 items-start" id="your-conlangs">
         <h2 className="font-bold text-2xl mb-4">Your Conlangs:</h2>
         <ConlangsList authOnly user={uname} />
@@ -63,7 +74,7 @@ export default async function DashboardPage() {
         <ConlangsList />
       </div>
 
-      <PatreonFloatingCard/>
+      <PatreonFloatingCard />
     </div>
   );
 }
